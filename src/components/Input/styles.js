9b2e615fd01a15817550d82849f9592547616155
vitor hugo <vitor.weve.123@gmail.com>
2026-02-09 19:45:39 -0300

    Calculadora feita com React

---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  wight: 100%;
+  height: 75px;
+  background-color: #696969;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #696969;
+    border: 0;
+    float: right;
+    clear: both;
+    padding: 0 10px;
+    font-size: 24px;
+    font-family: "Roboto";
+    color: #ffff;
+  }
+`;
